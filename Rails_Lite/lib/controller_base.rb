5dbd31808b0b557bb277a1a397ceef0488192580
dmyasnyankin <dmyasnyankin@berkeley.edu>
2018-11-21 16:44:22 -0800

    three phases

--- Rails_Lite/lib/controller_base.rb
+++ Rails_Lite/lib/controller_base.rb
@@ -8,33 +8,60 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req = req
+    @res = res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    if !@already_built_response
+      @res.status = 302
+      @res['Location'] = url
+      @already_built_response = true
+      session.store_session(@res)
+    else
+      raise_error "Cant redirect twice"
+    end
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    if !@already_built_response
+      @res['Content-Type'] = content_type
+      @res.write(content)
+      @already_built_response = true
+      session.store_session(@res)
+    else
+      raise_error "Cant do that"
+    end
   end
 
   # use ERB and binding to evaluate templates
   # pass the rendered html to render_content
   def render(template_name)
+    path = File.dirname(__FILE__)
+    new_path = File.join(path, "../views/cats_controller", "#{template_name}.html.erb")
+    file_content = File.read(new_path)
+    erb_code = ERB.new(file_content).result(binding)
+    render_content(erb_code, "text/html")
   end
 
   # method exposing a `Session` object
   def session
+    @session ||= Session.new(@req)
   end
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    
   end
 end
 
